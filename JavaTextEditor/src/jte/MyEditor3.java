4f4dcdd3058898cf60c895a805688db387374f21
kek-sec <georgepetrakis99@gmail.com>
2022-01-08 12:41:30 +0200

    docs :newspaper:

--- JavaTextEditor/src/jte/MyEditor3.java
+++ JavaTextEditor/src/jte/MyEditor3.java
@@ -1,12 +1,18 @@
-
+/**
+ * @author Petrakis Georgios , tp4768 
+ * @repository https://github.com/kek-Sec/JavaTextEditor
+ * 
+ */
 package jte;
-
 import javax.swing.SwingUtilities;
 import jre.Utilities.BuildForm;
 public class MyEditor3 {
 
     public static void main(String[] args) {
-        // ENTRANCE POINT
+        /**
+         * Entry point for creating the main frame
+         * 
+         */
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new BuildForm().Build();
